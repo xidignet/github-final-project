b547cf2b4a01d86caa5c4d4f4296eff5f17c9bed
xidignet <38257379+xidignet@users.noreply.github.com>
2023-05-13 00:53:34 -0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -3,7 +3,8 @@
 # annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
-# Additional Authors: xidignet
+# Additional Authors: 
+# xidignet
 # Input:
 # p, principal amount
 # t, time period in years
